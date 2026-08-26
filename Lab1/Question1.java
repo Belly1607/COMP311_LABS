e5cdc658f3f9dbada79e528f0dbb0f5c147dd17e
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-08-26 16:39:34 +0200

    Added  Question 1: personal profile

---
+++ Lab1/Question1.java
@@ -0,0 +1,39 @@
+/**
+ * NAME: Esabel Mutisi
+ * STUDENT ID: 2200423
+ */
+
+
+public class Question1 {
+    public static void main(String[]args) {
+        //declare a variable called name, age, GPA,fullTime
+
+        String name;
+        int age;
+        double GPA;
+        boolean fullTime;
+         
+        //assign variable name with a name called "Esabel Mutisi"
+
+        name = "Esabel Mutisi";
+
+        //assign variable age with 20
+
+        age = 21;
+
+        //assign variable GPA with 3.6
+
+        GPA = 4.45;
+
+        //assign variable full-time with true
+        fullTime = true;
+
+        //print out the values of name,age,GPA,full-time,course and studentID
+
+        System.out.println("Name: " + name); //print out the value of name
+        System.out.println("Age: " + age);   //print out the value of age
+        System.out.println("GPA: " + GPA);   //print out the value of GPA
+        System.out.println("Full-time: " + fullTime); //print out the value of full-time
+        
+    }
+}
