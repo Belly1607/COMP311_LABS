550b59aaad9dbde6ca4b82abae0085247f129218
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-08-28 10:08:29 +0200

    Question 3: greeting program

---
+++ Question3.java
@@ -0,0 +1,40 @@
+/**
+ * Name: Esabel Mutisi
+ * Student ID: 24020114
+ * Question 3
+*/
+
+//import scanner from java.util
+        import java.util.* ;
+
+public class Question3 {
+    public static void main(String []args) {
+        // declare scanner
+        Scanner scan = new Scanner ( System.in);
+
+        //print out a message to the user asking fot their name
+        System.out.print("Please enter your name: ");
+
+        // declare a variable called name and it reads the name from the user
+        String name = scan.nextLine();
+
+        //print out a message to the user asking fot their age
+        System.out.print("Please enter your age: ");
+
+        // declare a variable called age and it reads the age from the user
+        int age = scan.nextInt();
+
+        //declare a variable called futureAge 
+        int futureAge;
+
+        //calculate the age of the user after 5 years
+        futureAge = age + 5;
+
+        // declare a string variable called sentence which reads the name and future age of the user
+        String sentence = "Hello " + name + "! In five years, you will be " + futureAge + " years old";
+        
+        //displays the sentence
+        System.out.println(sentence);
+    }
+
+}
