77e87a95d47e42fb6a142489991b7c80d27fad40
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-08-28 09:02:22 +0200

    Question 2: Primitive Playground

---
+++ Lab1/Question2.java
@@ -0,0 +1,31 @@
+/**
+ * NAME: Esabel Mutisi
+ * STUDENT ID: 24020114
+ * Question 2
+ */
+
+public class Question2 {
+    public static void main(String[]args) {
+        /* declare variables called myByte, shortValue, number, longValue, piValue, pi, myChar, and isTrue
+         assign values on the variables */
+
+        byte myByte = 100;
+        short shortValue = 30000;
+        int number = 400000;
+        long longValue = 5000341267L;
+        float piValue = 3.14f;
+        double pi = 3.141592653;
+        char myChar = 'E';
+        boolean isTrue = true; 
+
+        //print out the values of myByte, shortValue, number, longValue, piValue, pi, myChar, and isTrue
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + number);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + piValue);
+        System.out.println("double: " + pi);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + isTrue);
+    }
+}
