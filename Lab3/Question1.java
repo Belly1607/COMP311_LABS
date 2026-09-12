96eaefe2dd4fdc46338edc54ede9e2e1b62c872f
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-09-12 16:25:13 +0200

    Question 1 : While Loop: countdown

---
+++ Lab3/Question1.java
@@ -0,0 +1,26 @@
+/**
+ * Name: Esabel Mutisi
+ * Student ID: 24020114
+ * Question 1 : While Loop: countdown
+ */
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        //declare an variable called number then initialize at 10 where the countdown starts 
+        int number = 10;
+
+        // use the while loop to repeat the number is greater than 0
+        while(number > 0){
+
+            // Display the current countdown number
+            System.out.println(number);
+
+            // Decrease the number by 1 after each loop
+            number--;
+        }
+
+        // Display the final message after the countdown finishes
+        System.out.print("Liftoff!");
+    }
+}
