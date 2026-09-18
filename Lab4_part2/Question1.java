0af39b06f8474ede6b8ac8f3e61fa1d5123067e9
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-09-18 14:29:21 +0200

    Question 1 — Declaring and Initialising an Array

---
+++ Lab4_part2/Question1.java
@@ -0,0 +1,20 @@
+package COMP311_LABS.Lab4_part2;
+
+/**
+ * Name: Esabel Mutisi
+ * Student ID: 24020114
+ * Question 1 — Declaring and Initialising an Array
+ */
+
+// Import all utility classes from java.util, including Arrays
+import java.util.*;
+public class Question1 {
+    public static void main(String[] args) {
+
+        //declare and initialize an integer array with seven temperature values
+        int [] arr = {27, 30, 2, 0, 27, -6, 12};
+
+         //display the whole array in a readable format using Arrays.toString()
+        System.err.println(Arrays.toString(arr));
+    }
+}
