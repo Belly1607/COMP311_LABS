7007e731db51cbdcebf2d106b466abed76afd24e
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-09-20 13:00:10 +0200

    Question 1 — Creating a Class

---
+++ Lab5/Book.java
@@ -0,0 +1,14 @@
+package COMP311_LABS.Lab5;
+
+/**
+ * Name: Esabel Mutisi
+ * Student ID: 24020114
+ * Question 1 — Creating a Class
+ */
+
+public  class Book{
+    private String title;
+    private String author;
+    private double price;
+}
+
