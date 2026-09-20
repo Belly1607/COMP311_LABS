016abce2eb57ef95f1e58ca1d44d8341fa803694
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-09-20 13:05:28 +0200

    Question 2 — Adding a Constructor

--- Lab5/Book.java
+++ Lab5/Book.java
@@ -4,11 +4,18 @@ package COMP311_LABS.Lab5;
  * Name: Esabel Mutisi
  * Student ID: 24020114
  * Question 1 — Creating a Class
+ * Question 2 — Adding a Constructor
  */
 
 public  class Book{
     private String title;
     private String author;
     private double price;
+
+    public Book(String title, String author, double price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
 }
 
